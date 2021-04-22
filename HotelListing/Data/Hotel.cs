ab89bd9129ee873936392df4d350e338b89849f6
adam watson <adamwatson08@gmail.com>
2021-04-21 20:18:20 -0500

    added seed data

--- HotelListing/Data/Hotel.cs
+++ HotelListing/Data/Hotel.cs
@@ -11,6 +11,8 @@ namespace HotelListing.Data
         public int Id { get; set; }
         public string Name { get; set; }
         public double Rating { get; set; }
+        public string Address { get; set; }
+
         [ForeignKey(nameof(Country))]
         public int CountryId { get; set; }
         public Country Country { get; set; }
